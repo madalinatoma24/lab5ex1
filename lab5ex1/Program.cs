82d692544e63e4d422107acffed6a82ed5e969aa
madalina.toma <madalina.toma@softem.ro>
2023-01-22 22:55:23 +0200

    Add project files.

---
+++ lab5ex1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lab5ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Ex1: Scrieti un program care va afisa pozitia unui substring intr-un string, 
+            ambele siruri de caractere fiind citite de la tastatura
+             */
+
+            Console.WriteLine("Introduceti string-ul");
+            var text = Console.ReadLine();
+
+            Console.WriteLine("Introduceti substring-ul");
+            var substring = Console.ReadLine();
+
+            var position = text.IndexOf(substring);
+            Console.WriteLine(position);
+        }
+    }
+}
